d0f99ab87ef400f25dcfa527f77b5496b655fcc7
yasmin aparecida cordeiro <yasmin.aparecida.cordeiro>
2024-05-24 12:15:52 -0300

    lua

---
+++ APP.js
@@ -0,0 +1,12 @@
+let priNum = prompt('Digite um número:');
+let segNum = prompt('Digite outro número:');
+priNum = parseFloat (priNum);
+segNum = parsefloat (segNum);
+let soma = priNum + segNum;
+let sub = priNum - segNum;
+
+alert ('somarei os valores informados...');
+alert ('a soma de '+priNum+' e '+ segNum +' é '+ soma)
+
+alert ('Agora vou subtrai-los...')
+alert('a subtrção entre '+priNum +' e '+ segNum +' é '+ sub)
